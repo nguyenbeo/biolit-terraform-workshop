ced0899164747af92f3b060ec6d054f72ca1ea85
Nguyen Binh Nguyen <nguyenbeo@gmail.com>
2026-04-03 00:30:37 +0200

    Use provider random to generate password

--- 1.local_file.tf
+++ 1.local_file.tf
@@ -4,7 +4,14 @@ terraform {
 
 provider "local" {}
 
-resource "local_file" "hello" {
-  filename = "./resources/hello.txt"
-  content  = "Hello world"
+provider "random" {}
+
+resource "random_password" "email_random_password" {
+  length = 16
+  special = true
+}
+
+resource "local_file" "password_file" {
+  filename = "./resources/password.txt"
+  content  = "Password: ${random_password.email_random_password.result}"
 }
